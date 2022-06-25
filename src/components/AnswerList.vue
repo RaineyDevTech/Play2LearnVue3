1f659362d76f00e7cb87dce8fa6d69bb821e1b54
JD <cogscidawg@gmail.com>
2022-06-25 12:18:37 -0400

    list is working

--- src/components/AnswerList.vue
+++ src/components/AnswerList.vue
@@ -1,22 +1,37 @@
 <template>
+  <div>
   <ol >
-      <li >Booty for Dinner MotherFucker</li>
+      <li v-for="answers in answerList" :key="answers">{{answers}}</li>
+      <li>Eat my ass motherfucker</li>
 
   </ol>
+  <p>the list of correct answers is : {{answerList}}</p>
+
+  <ol>
+  
+  </div>
 </template>
 
 <script>
 
 //import Word from './Word';
-
+import answerList from './Main';
   export default {
     // eslint-disable-next-line
     name: 'AnswerList',
     components: {
       
     },
-    props: {
+    data: function(){
+      return{
       answerList: Array
+      }
+    },
+    methods: {
+      setList(){
+        this.answerList = answerList;
+      }
+
     }
   }
 </script>
